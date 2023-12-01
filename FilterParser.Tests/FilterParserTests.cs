3de181a4b6713a29b233efc05b99ce31ccaf29bc
mohammad <mohmmad.hraiba@appsfactory.de>
2023-12-01 09:08:33 +0100

    More Unit tests

--- FilterParser.Tests/FilterParserTests.cs
+++ FilterParser.Tests/FilterParserTests.cs
@@ -39,6 +39,8 @@ public class FilterParserTests
         //Act
         var result = FilterParserLib.FilterParser.ApplyFilter(students, filter);
         var enumerateResult = result.ToList();
+        
+        // Assert
         Assert.NotNull(result);
         Assert.IsAssignableFrom<IQueryable>(result);
         Assert.NotEmpty(enumerateResult);
@@ -77,6 +79,8 @@ public class FilterParserTests
         //Act
         var result = FilterParserLib.FilterParser.ApplyFilter(students, filter);
         var enumerateResult = result.ToList();
+        
+        // Assert
         Assert.NotNull(result);
         Assert.IsAssignableFrom<IQueryable>(result);
         Assert.NotEmpty(enumerateResult);
@@ -103,6 +107,8 @@ public class FilterParserTests
         //Act
         var result = FilterParserLib.FilterParser.ApplyFilter(students, filter);
         var enumerateResult = result.ToList();
+        
+        // Assert
         Assert.NotNull(result);
         Assert.IsAssignableFrom<IQueryable>(result);
         Assert.NotEmpty(enumerateResult);
@@ -124,12 +130,14 @@ public class FilterParserTests
         //Act
         var result = FilterParserLib.FilterParser.ApplyFilter(students, filter);
         var enumerateResult = result.ToList();
+        
+        // Assert
         Assert.NotNull(result);
         Assert.IsAssignableFrom<IQueryable>(result);
         Assert.NotEmpty(enumerateResult);
         Assert.Equal(3, result.Count());
     }
-    
+
     [Fact]
     public void Given_Null_Filter_And_Null_DataSet_Throw_NullArgumentException()
     {
@@ -139,6 +147,103 @@ public class FilterParserTests
 
         //Act && Assert
         Assert.Throws<ArgumentNullException>(() => FilterParserLib.FilterParser.ApplyFilter(students, filter));
+    }
 
+    [Fact]
+    public void Given_Filter_With_And_Or_Operators_Return_IQueryable()
+    {
+        //Arrange
+        const string filter = """
+                              {
+                                "filters": [
+                                  {
+                                    "filters":[
+                                    {
+                                    "field": "LastName",
+                                    "operation": "contains",
+                                    "value": "Hr"
+                                    },
+                                      {
+                                        "field": "Name",
+                                        "operation": "equal",
+                                        "value": "Mohammad"
+                                      }
+                                    ],
+                                    "operator": "and"
+                                  },
+                                  {
+                                    "operation": "equal",
+                                    "field": "Mark",
+                                    "value": 8
+                                  }
+                                ],
+                                "operator": "or"
+                              }
+                              """;
+        var students = new List<Student>()
+        {
+            new() {Name = "Mohammad", LastName = "Hraiba", Mark = 9},
+            new() {Name = "Louay", LastName = "Mo", Mark = 8},
+            new() {Name = "Johndoe", LastName = "smith", Mark = 1}
+        };
+
+        //Act
+        var result = FilterParserLib.FilterParser.ApplyFilter(students, filter);
+        var enumerateResult = result.ToList();
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IQueryable>(result);
+        Assert.NotEmpty(enumerateResult);
+        Assert.Equal(2, result.Count());
+    }
+    
+    [Fact]
+    public void Given_Filter_With_Or_And_Operators_Return_IQueryable()
+    {
+        //Arrange
+        const string filter = """
+                              {
+                                "filters": [
+                                  {
+                                    "filters":[
+                                    {
+                                    "field": "LastName",
+                                    "operation": "equal",
+                                    "value": "smith"
+                                    },
+                                      {
+                                        "field": "Name",
+                                        "operation": "equal",
+                                        "value": "Mohammad"
+                                      }
+                                    ],
+                                    "operator": "or"
+                                  },
+                                  {
+                                    "operation": "equal",
+                                    "field": "Mark",
+                                    "value": 8
+                                  }
+                                ],
+                                "operator": "and"
+                              }
+                              """;
+        var students = new List<Student>()
+        {
+            new() {Name = "Mohammad", LastName = "Hraiba", Mark = 9},
+            new() {Name = "Louay", LastName = "Mo", Mark = 8},
+            new() {Name = "Johndoe", LastName = "smith", Mark = 1}
+        };
+
+        //Act
+        var result = FilterParserLib.FilterParser.ApplyFilter(students, filter);
+        var enumerateResult = result.ToList();
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IQueryable>(result);
+        Assert.Empty(enumerateResult);
+        Assert.Equal(0, result.Count());
     }
 }
--- FilterParserLib/Filter.cs
+++ FilterParserLib/Filter.cs
@@ -2,4 +2,9 @@ using System.Text.Json;
 
 namespace FilterParserLib;
 
-internal  record Filter(string Field, Operation Operation, JsonElement Value, Operator Operator, IEnumerable<Filter>? Filters);
+internal record Filter(
+    string Field,
+    Operation Operation,
+    JsonElement Value,
+    Operator Operator,
+    IEnumerable<Filter>? Filters);
